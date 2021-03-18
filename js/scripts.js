e69f489acdc1836e8968d829f95de47f1f6ff013
dojo24 <mlongenberger@codingdojo.com>
2021-03-17 20:37:49 -0400

    saving

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         for(var i = 0; i < res.data.length; i++) {
             html_str += "<div class='tunes'>";
             html_str += "<h3>" + res.data[i].name + "</h3>";
-            html_str += "<img src='" + res.data[i].img +"' alt='" + res.data[i].name + "' />"
+            html_str += "<img src='" + res.data[i].img + "' alt='" + res.data[i].name + "' />"
             html_str += "</div>";
         }
         $(".main").html(html_str);
