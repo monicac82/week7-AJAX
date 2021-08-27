f4e9abd0ba8bdfbc0e751b15c512cd8c597b13e3
Monica Colling <monicavictor82@gmail.com>
2021-08-27 16:26:45 -0700

    started api calls

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
-    $.get("https://looney-toons-api.herokuapp.com/api/characters", function(res) {
+$(document).ready(function () {
+    $.get("https://looney-toons-api.herokuapp.com/api/characters", function (res) {
         console.log(res.data)
         var html_str = "";
-        for(var i = 0; i < res.data.length; i++) {
-            html_str += "<div class='tunes'>";
-            html_str += "<h3>" + res.data[i].name + "</h3>";
+        for (var i = 0; i < res.data.length; i++) {
+            html_str += "<div class ='tunes'>"
+            html_str += "<h3>" + res.data[i].name + "</h3>"
             html_str += "<img src='" + res.data[i].img + "' alt='" + res.data[i].name + "' />"
-            html_str += "</div>";
+            html_str += "</div>"
         }
         $(".main").html(html_str);
-    }, "json");
+    }, "json")
 })
